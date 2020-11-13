f48ac0c17ca2ab3ffd00a51548fc860ac7fcdf2c
marion2614 <74400663+marion2614@users.noreply.github.com>
2020-11-13 10:02:17 +0100

    Update Produit.h

--- Produit.h
+++ Produit.h
@@ -5,29 +5,29 @@
 #include <string>
 #include <vector>
 
-//Création de la classe Produit 
+// Création de la classe Produit 
 class Produit{
 
 public:
 
-	//Constructeur
+	// Constructeur
 	Produit(std::string titre = "PS4", 
 			std::string description = "Console de jeu Sony",
 			int quantite=1, float prix=0.0);
 
-	//Getters
+	// Getters
 	std::string getTitre() const;
 	std::string getDescription() const;
 	int getQuantite() const;
 	float getPrixU() const;
 
-	//setters
+	// Setters
 	void setQuantite(int quantite);
 
 
 private:
 
-	//attributs de Produit
+	// Attributs de Produit
 	std::string m_titre;
 	std::string m_description;
 	int m_quantite;
@@ -37,7 +37,7 @@ private:
 
 };
 
-//surcharge de l'opérateur <<
+// Surcharge de l'opérateur 
 std::ostream& operator << (std::ostream& output, Produit& obj);
 
 
