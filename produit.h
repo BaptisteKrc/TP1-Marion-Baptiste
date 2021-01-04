13257608b3d8877079de998e440d9fb006b680c5
BaptisteKrc <62386237+BaptisteKrc@users.noreply.github.com>
2021-01-04 13:28:35 +0100

    Create produit.h

---
+++ produit.h
@@ -0,0 +1,47 @@
+//produit.h
+
+#ifndef _PRODUIT_H
+#define _PRODUIT_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Création de la classe Produit 
+class Produit{
+
+public:
+
+	// Constructeur
+	Produit(std::string titre = "PS4", 
+			std::string description = "Console de jeu Sony",
+			int quantite=1, float prix=0.0);
+
+	// Getters
+	std::string getTitre() const;
+	std::string getDescription() const;
+	int getQuantite() const;
+	float getPrixU() const;
+
+	// Setters
+	void setQuantite(int quantite);
+
+
+private:
+
+	// Attributs de Produit
+	std::string m_titre;
+	std::string m_description;
+	int m_quantite;
+	float m_prixU;
+
+
+
+};
+
+// Surcharge de l'opérateur 
+std::ostream& operator << (std::ostream& output, Produit& obj);
+
+
+
+#endif
