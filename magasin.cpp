5bb522f07d7b18049ec787915dda4f889ac01165
BaptisteKrc <62386237+BaptisteKrc@users.noreply.github.com>
2021-01-17 12:24:34 +0100

    Create magasin.cpp

---
+++ magasin.cpp
@@ -0,0 +1,25 @@
+//Magasin.cpp
+
+#include <iostream>
+#include <vector>
+
+#include "Magasin.h"
+#include "Produit.h"
+
+Magasin::Magasin() {}
+
+
+void Magasin::ajoutProduit(Produit* produit) {
+    m_produit.push_back(produit);
+}
+
+void Magasin::display() {
+    for (int i = 0; i < produit.size(); i++) {
+        std::cout << "NAME 		DESCRIPTION 		QUANTITY		PRICE" << std::endl;
+        std::cout << m_produit.at(i) << std::endl;
+    }
+}
+
+void Magasin::modificationQ(int n, Produit* produit) {
+    produit.setQuantite(n);
+}
