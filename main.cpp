9dd1067424cfb9d6c010c727183bfcbf20e7dffe
BaptisteKrc <62386237+BaptisteKrc@users.noreply.github.com>
2021-01-25 12:39:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #define ANSI_RED   "\033[0;91m"
 #define ANSI_RESET "\033[0;0m"
 #define ANSI_CYAN "\033[0;36m"
+#define ANSI_BLUE "\033[34m"
 
 #define SLEEP_DELAY 2
 
@@ -21,6 +22,7 @@ void move(int x, int y){
     cout << escape_ANSI.c_str();
 }
 
+//Création du titre du magasin sur l'écran d'accueil
 void Box(){
 
     struct winsize size;
@@ -36,11 +38,11 @@ void Box(){
 
     // Tracé de la case en bas a droite
     move(x,y);
-    cout << ANSI_RED << top.c_str();
+    cout << ANSI_CYAN << top.c_str();
     move(x,y+1);
-    cout << ANSI_RED << mid.c_str();
+    cout << ANSI_CYAN << mid.c_str();
     move(x,y+2);
-    cout << ANSI_RED << bot.c_str();
+    cout << ANSI_CYAN << bot.c_str();
 
     move(x+5, y+1);
 }
@@ -72,47 +74,45 @@ void displayAccueil() {
 //Affichage du menu principal
 void displayMenu(){
     system("clear");
-    cout << "1 - Gestion des stocks" << endl;
-    cout << "2 - Gestion des clients" << endl;
-    cout << "3 - Gestion des commandes" << endl;
-    cout << "4 - Sauvegarder le magasin" << endl;
-    cout << "5 - Charger un magasin" << endl;
-    cout << "6 - Retour à l'accueil" << endl;
+    cout << ANSI_GREEN << "1 - Gestion des stocks" << ANSI_RESET << endl;
+    cout << ANSI_BLUE << "2 - Gestion des clients" << ANSI_RESET << endl;
+    cout << ANSI_RED << "3 - Gestion des commandes" << ANSI_RESET << endl;
+    cout << ANSI_CYAN << "4 - Retour à l'accueil" << ANSI_RESET  << endl;
     Box();
 }
 
 //Affichage menu client
 void displayMenuClient() {
     system("clear");
-    cout << "1 - Ajouter un client" << endl;
-    cout << "2 - Afficher la liste des clients" << endl;
-    cout << "3 - Afficher un client grâce à son ID" << endl;
-    cout << "4 - Afficher un client grâce à son nom et son prénom" << endl;
-    cout << "5 - Retour au menu principal" << endl;
+    cout << ANSI_BLUE << "1 - Ajouter un client"<< ANSI_RESET << endl;
+    cout << ANSI_BLUE << "2 - Afficher la liste des clients"<< ANSI_RESET << endl;
+    cout << ANSI_BLUE << "3 - Afficher un client grâce à son ID"<< ANSI_RESET << endl;
+    cout << ANSI_BLUE << "4 - Afficher un client grâce à son nom et son prénom"<< ANSI_RESET << endl;
+    cout << ANSI_CYAN << "5 - Retour au menu principal"<< ANSI_RESET << endl;
     Box();
 }
 
 //Affichage menu commande
 void displayMenuCommande() {
     system("clear");
-    cout << "1 - Afficher la liste des commandes" << endl;
-    cout << "2 - Valider une commande grâce à son ID" << endl;
-    cout << "3 - Afficher la liste des commandes validées" << endl;
-    cout << "4 - Afficher une commande grâce à son ID" << endl;
-    cout << "5 - Afficher toutes les commandes d'un client grâce à son ID" << endl;
-    cout << "6 - Afficher toutes les commandes d'un client grâce à son nom et son prénom" << endl;
-    cout << "7 - Retour au menu principal" << endl;
+    cout<< ANSI_RED << "1 - Afficher la liste des commandes"<< ANSI_RESET << endl;
+    cout<< ANSI_RED << "2 - Valider une commande grâce à son ID" << ANSI_RESET<< endl;
+    cout<< ANSI_RED << "3 - Afficher la liste des commandes validées"<< ANSI_RESET << endl;
+    cout<< ANSI_RED << "4 - Afficher une commande grâce à son ID"<< ANSI_RESET << endl;
+    cout<< ANSI_RED << "5 - Afficher toutes les commandes d'un client grâce à son ID"<< ANSI_RESET << endl;
+    cout<< ANSI_RED << "6 - Afficher toutes les commandes d'un client grâce à son nom et son prénom"<< ANSI_RESET << endl;
+    cout<< ANSI_CYAN << "7 - Retour au menu principal" << ANSI_RESET << endl;
     Box();
 }
 
 //Affichage menu magasin/stock
 void displayMenuMagasin() {
     system("clear");
-    cout << "1 - Ajouter un produit" << endl;
-    cout << "2 - Modifier la quantité d'un produit" << endl;
-    cout << "3 - Afficher tous les produits du magasin" << endl;
-    cout << "4 - Afficher les détails d'un produit grâce à son nom" << endl;
-    cout << "5 - Retour au menu principal" << endl;
+    cout << ANSI_GREEN  << "1 - Ajouter un produit" << ANSI_RESET << endl;
+    cout << ANSI_GREEN << "2 - Modifier la quantité d'un produit" << ANSI_RESET << endl;
+    cout << ANSI_GREEN << "3 - Afficher tous les produits du magasin" << ANSI_RESET << endl;
+    cout << ANSI_GREEN << "4 - Afficher les détails d'un produit grâce à son nom" << ANSI_RESET << endl;
+    cout << ANSI_CYAN << "5 - Retour au menu principal" << ANSI_RESET << endl;
     Box();
 }
 
@@ -129,38 +129,35 @@ int getInput(){
 }
 
 void press_to_continue() {
-    cout << ANSI_GREEN << "Appuyer sur ENTREE pour continuer ..." << ANSI_RESET << endl;
-    system("read var");
-}
-
-void to_be_implemented_next() {
-    system("clear");
-    cout << ANSI_RED << "Cette option n'est malheureusement pas encore disponible" << endl;
-    cout << "Appuyer sur ENTREE pour revenir à l'accueil ..." << ANSI_RESET << endl;
+    cout << ANSI_CYAN<< "Appuyer sur ENTREE pour continuer ..." << ANSI_RESET << endl;
     system("read var");
 }
 
 void initialisation_store(Magasin* shop){
-    Produit* p3 = new Produit("Xbox One", "Console de jeu Microsoft", 20, 179.99);
-    Produit* p = new Produit("PS4 ", "Console de jeu Sony", 20, 249.99);
-    Produit* p4 = new Produit("PS5", "Console de jeu Sony", 20, 599.99);
-    Produit* p2 = new Produit("Switch", "Console de jeu Nintendo", 20, 299.99);
+    Produit* p3 = new Produit("Xbox One", "Console de jeu Microsoft", 15, 179.99);
+    Produit* p = new Produit("PS4 ", "Console de jeu Sony", 8, 249.99);
+    Produit* p4 = new Produit("PS5", "Console de jeu Sony", 14, 599.99);
+    Produit* p2 = new Produit("Switch", "Console de jeu Nintendo", 30, 299.99);
     Produit* p5 = new Produit("3DS", "Console de jeu Nintendo", 20, 299.99);
+	Produit* p6 = new Produit("PC", "Ordinateur portable", 10, 799.90);
 
     shop->addProduit(p);
     shop->addProduit(p2);
     shop->addProduit(p3);
     shop->addProduit(p4);
     shop->addProduit(p5);
+    shop->addProduit(p6);
 
     Client* client = new Client(shop->generateClientID(), "Ginhac", "Dominique", shop->getProduits());
     Client* client2 = new Client(shop->generateClientID(), "Krecina", "Baptiste", shop->getProduits());
     Client* client3 = new Client(shop->generateClientID(), "Paris", "Marion", shop->getProduits());
-
+    Client* client4 = new Client(shop->generateClientID(), "Senechal", "Hugo", shop->getProduits());
+    
     shop->addClient(client);
     shop->addClient(client2);
     shop->addClient(client3);
-
+    shop->addClient(client4);
+    
     Commande* co = new Commande(client, client->getPanier(), "En cours de traitement", shop->generateOrderID());
     Commande* co2 = new Commande(client2, client->getPanier(), "En cours de traitement", shop->generateOrderID());
 
@@ -217,7 +214,7 @@ int main(){
                                         cin >> prixU;
                                         Produit* p = new Produit(nom, desc, quantite, prixU);
                                         easystore.addProduit(p);
-                                        cout << ANSI_GREEN << "Le produit a été ajouté" << ANSI_RESET << endl;
+                                        cout << ANSI_CYAN << "Le produit a été ajouté" << ANSI_RESET << endl;
                                         sleep(SLEEP_DELAY);
                                         displayMenuMagasin();
                                         break;
@@ -231,7 +228,7 @@ int main(){
                                         cout << "Nouvelle quantité : ";
                                         int quantite = getInput();
                                         easystore.updateProductQuantity(easystore.getProduit(nom), quantite);
-                                        cout << ANSI_GREEN << "Le produit a été modifié" << ANSI_RESET << endl;
+                                        cout << ANSI_CYAN << "Le produit a été modifié" << ANSI_RESET << endl;
                                         displayMenuMagasin();
                                         break;
                                     }
@@ -282,7 +279,7 @@ int main(){
                                         getline(cin, nom);
                                         Client* c = new Client(id,nom, prenom, panier);
                                         easystore.addClient(c);
-                                        cout << ANSI_GREEN << "Le client a été ajouté" << ANSI_RESET << endl;
+                                        cout << ANSI_CYAN << "Le client a été ajouté" << ANSI_RESET << endl;
                                         sleep(SLEEP_DELAY);
                                         displayMenuClient();
                                         break;
@@ -347,7 +344,7 @@ int main(){
                                         if ( easystore.getCommande(id) != nullptr) {
                                             easystore.validerCommande(c);
                                             if (easystore.getCommande(id)->getStatut() == "Validee") {
-                                                cout << ANSI_GREEN << "La commande a été validée" << ANSI_RESET << endl;
+                                                cout << ANSI_CYAN << "La commande a été validée" << ANSI_RESET << endl;
                                             } else {
                                                 cout << ANSI_RED << "La commande n'a pas été validée" << ANSI_RESET << endl;
                                             }
@@ -408,15 +405,7 @@ int main(){
                                 }
                             }
                             break;
-                        case 4 : // Sauvegarder le magasin                                        // Pas finis
-                            to_be_implemented_next();
-                            quit2 = true;
-                            break;
-                        case 5 : // Charger un magasin                                            // Pas finis
-                            to_be_implemented_next();
-                            quit2 = true;
-                            break;
-                        case 6 : // Retour à l'accueil
+                        case 4 : // Retour Ã  l'accueil
                             quit2 = true;
                             break;
                         default:
