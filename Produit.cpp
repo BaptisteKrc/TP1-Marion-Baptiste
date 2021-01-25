6e856e2e4bfd259dc64782e192a8f59db1fd1166
BaptisteKrc <62386237+BaptisteKrc@users.noreply.github.com>
2021-01-25 16:41:45 +0100

    Update Produit.cpp

--- Produit.cpp
+++ Produit.cpp
@@ -1,39 +1,38 @@
 #include "Produit.h"
 
 Produit::Produit() {}
-
+//definition d'un produit : il lui faut un titre, une description etc..
 Produit::Produit(string titre, string desc, int quantite, double prix) {
     this->m_titre = titre;
     this->m_description = desc;
     this->m_quantite = quantite;
     this->m_prix_unitaire = prix;
 }
-
+//getter
 string Produit::getTitre()  {
     return m_titre;
 }
-
+//getter
 string Produit::getDescription()  {
     return m_description;
 }
-
+//getter
 int Produit::getQuantite()  {
     return m_quantite;
 }
-
+//getter
 double Produit::getPrixUnitaire()  {
     return m_prix_unitaire;
 }
-
+//fonction pour modifier la quantité 
 void Produit::modifierQuantite(int quantite){
     this->m_quantite = quantite;
 }
 
-
 string Produit::toString() {
-    return  m_titre + "\t\t" + m_description + "\t\t(" + to_string(m_quantite) +")\t\t" + to_string(m_prix_unitaire) + " €";
+    return  m_titre + "\t\t" + m_description + "\t\t(" + to_string(m_quantite) +")\t\t" + to_string(m_prix_unitaire) + " â‚¬";
 }
-
+//surcharge de l'opérateur << pour afficher un produit 
 ostream&operator<< (ostream&output, Produit produit){
     output << produit.toString() << endl;
 }
