d6e32f389816c15be07814bcc7be1686716cde3b
marion2614 <74400663+marion2614@users.noreply.github.com>
2021-01-28 13:31:36 +0100

    Update Commande.cpp

--- Commande.cpp
+++ Commande.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 Commande::Commande() : m_client(), m_produits(), m_statut("Non renseigne"), m_numero(0) {}
 
-
+//Constructeur 
 Commande::Commande(Client* client, vector<Produit*> produits, string statut, int numero) {
     this->m_client = client;
     this->m_produits = produits;
@@ -15,40 +15,41 @@ Commande::Commande(Client* client, vector<Produit*> produits, string statut, int
     this->m_numero = numero;
 }
 
-
+//Setter Client
 void Commande::setClient(Client* client){
 	this->m_client = client;
 }
 
-
+//Setter Produit
 void Commande::setProduits(vector<Produit*> produits){
 	this->m_produits = produits;
 }
 
+//Setter statue (de la commande)
 void Commande::setStatut(string statut){
 	this->m_statut = statut;
 }
-
+//Setter numéro (de la commande)
 void Commande::setNumero(int numero){
 	this->m_numero = numero;
 }
-
+//Getter Client
 Client* Commande::getClient(){
 	return this->m_client;
 }
-
+//Getter Produit
 vector<Produit*> Commande::getProduits(){
 	return this->m_produits;
 }
-
+//Getter statut (de la commande)
 string Commande::getStatut(){
 	return this->m_statut;
 }
-
+//Getter numéro (de la commande)
 int Commande::getNumero(){
 	return this->m_numero;
 }
-
+//Permet de calculer le prix total de la commande
 double Commande::getPrixTotal() {
     double prixTotal = m_produits.at(0)->getPrixUnitaire() * m_produits.at(0)->getQuantite();
     double prixInt=0;
@@ -58,7 +59,7 @@ double Commande::getPrixTotal() {
     }
     return prixTotal;
 }
-
+//Surcharge de l'opérateur "<<" pour la commande
 ostream&operator<< (ostream&output,Commande commande){
 
     ConsoleTable table{"Nom","Description", "Quantite", "Prix"};
